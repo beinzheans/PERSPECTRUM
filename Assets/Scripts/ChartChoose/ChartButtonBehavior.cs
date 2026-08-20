d8812f97363baa4e196ec2d4e6dd96365ad2132f
beinzheans <raymondyeung1628@gmail.com>
2026-08-20 15:32:26 +0800

    wip: made chart selection screen more compact

--- Assets/Scripts/ChartChoose/ChartButtonBehavior.cs
+++ Assets/Scripts/ChartChoose/ChartButtonBehavior.cs
@@ -10,6 +10,7 @@ public class ChartButtonBehavior : ListBox
 {
     [SerializeField] private TMP_Text buttonText;
     [SerializeField] private TMP_Text difficultyText;
+    [SerializeField] private GameObject startTextButton;
     [SerializeField] private Image image;
 
     public ChartButtonBehaviorContents Contents { get; private set; }
@@ -17,9 +18,12 @@ public class ChartButtonBehavior : ListBox
     private const float k_DEFAULTALPHA = 0.2f;
 
     private int previousContentID = 0;
+
+    private bool isSelected = false;
     private void Start()
     {
         ChartChooseManager.ChartChooseInstance.OnChartButtonClicked += ChartChooseInstance_OnChartButtonClicked;
+
         previousContentID = ContentID;
     }
 
@@ -27,17 +31,26 @@ public class ChartButtonBehavior : ListBox
     {
         if (obj == null || id == -1)
         {
-            image.color = new Color(Color.white.r, Color.white.g, Color.white.b, k_DEFAULTALPHA);
+            isSelected = false;
+            HideSelectedVisuals();
             return;
         }
 
         if (ContentID != id)
         {
-            image.color = new Color(Color.white.r, Color.white.g, Color.white.b, k_DEFAULTALPHA);
+            isSelected = false;
+            HideSelectedVisuals();
             return;
         }
 
-        image.color = new Color(Color.yellow.r, Color.yellow.g, Color.yellow.b, k_DEFAULTALPHA);
+        if (isSelected) // this means we have double clicked on the instance!
+        {
+            ChartChooseManager.ChartChooseInstance.RequestPlayChart();
+            return;
+        }
+
+        isSelected = true;
+        ShowSelectedVisuals();
     }
 
     protected override void UpdateDisplayContent(IListContent content)
@@ -64,11 +77,24 @@ public class ChartButtonBehavior : ListBox
 
         if (ContentID != ChartChooseManager.ChartChooseInstance.CurrentSelectChartContentID)
         {
-            image.color = new Color(Color.white.r, Color.white.g, Color.white.b, k_DEFAULTALPHA);
+            isSelected = false;
+            HideSelectedVisuals();
             return;
         }
 
-        image.color = new Color(Color.yellow.r, Color.yellow.g, Color.yellow.b, k_DEFAULTALPHA);
+        isSelected = true;
+        ShowSelectedVisuals();
+    }
 
+    private void ShowSelectedVisuals()
+    {
+        image.color = new Color(Color.yellow.r, Color.yellow.g, Color.yellow.b, k_DEFAULTALPHA);
+        startTextButton.SetActive(true);
+    }
+
+    private void HideSelectedVisuals()
+    {
+        image.color = new Color(Color.white.r, Color.white.g, Color.white.b, k_DEFAULTALPHA);
+        startTextButton.SetActive(false);
     }
 }
